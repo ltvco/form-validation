0f1ced99ac15715deba1b403470eee011df76d31
Taylor Steele <tsteele@ltvco.com>
2023-12-20 09:25:05 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    filename: 'index.js',
+    library: {
+      type: 'umd',
+    },
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          compress: {
+            unused: true,
+            dead_code: true,
+            drop_debugger: true,
+            drop_console: true,
+            pure_funcs: ['console.log'],
+          },
+          format: {
+            comments: false,
+          },
+          keep_fnames: true,
+        },
+      }),
+    ],
+  },
+};
