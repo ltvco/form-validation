157f604d3842052fbb74b5fa2aee79cedc98fea9
Ale Soto <asoto@ltvco.com>
2024-07-19 10:48:54 -0600

    Add basic tests and foundations for future tests

---
+++ tests/4-methods.spec.ts
@@ -0,0 +1,4 @@
+/*
+Public Methods Testing
+- Check if each of the public methods can be called and work as expected
+*/
---
+++ tests/6-error-handling.spec.ts
@@ -0,0 +1,4 @@
+/*
+Error Handling Testing
+- Check if the library can handle errors gracefully, throw the correct errors, and provide the correct error messages
+*/
