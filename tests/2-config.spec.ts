b4046de79f36b304b0d646bd4716c16955f69881
Ale Soto <asoto@ltvco.com>
2024-07-19 10:48:54 -0600

    Add basic tests and foundations for future tests

---
+++ tests/2-config.spec.ts
@@ -0,0 +1,5 @@
+/*
+Configuration options testing
+- Check if each of the configuration options can be correctly applied and each one works as expected
+- No in-depth testing of Field options here
+*/
